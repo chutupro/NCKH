e212e417934a48078f3e7a2e5a03223cb16ccac3
huynhduyan15 <huynhduyan.300604@gmail.com>
2025-10-10 14:04:35 +0700

    ss
    sss

--- FrontEnd/src/pages/Home.jsx
+++ FrontEnd/src/pages/Home.jsx
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import React from 'react'
 import Banner from "../Component/home/Banner";
 import Exploration from "../Component/home/Exploration";
@@ -22,31 +21,4 @@ const Home = () => {
   )
 }
 
-=======
-import React from 'react'
-import Banner from "../Component/home/Banner";
-import Exploration from "../Component/home/Exploration";
-import Headers from "../Component/home/Headers";
-import Timeline from "../Component/home/Timeline";
-import Top from "../Component/home/Top";
-import Study from "../Component/home/Study";
-import Footer from "../Component/home/Footer";
-import Connect from "../Component/home/Connect";
-
-const Home = () => {
-  return (
-    <div>
-      <Headers />
-      <Banner />
-      <Exploration />
-      <Connect />
-      <Top />
-      <Study />
-      <Timeline />
-      <Footer />
-    </div>
-  )
-}
-
->>>>>>> Stashed changes
 export default Home
