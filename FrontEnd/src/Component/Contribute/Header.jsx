7004bea8893545794a4a4b653f4aa800cc722ccc
chutupro <vannghiaqng.2004@gmail.com>
2025-10-12 20:22:10 +0700

    nghiadone
    done

--- FrontEnd/src/Component/Contribute/Header.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import HomeHeaders from '../home/Headers';
-import '../../Styles/Contribute/Header.css';
-
-export default function Headers() {
-  // wrapper so pages can import from Component/contribute/Headers if desired
-  return <HomeHeaders />;
-}
