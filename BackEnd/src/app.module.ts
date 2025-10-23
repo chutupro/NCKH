3059ae2b49e1b5c3da0f6c3ad7e9ba16b1e00561
nguyenvanduc6093 <161592322+nguyenvanduc6093@users.noreply.github.com>
2025-10-23 15:26:42 +0700

    123
    3213123123

--- BackEnd/src/app.module.ts
+++ BackEnd/src/app.module.ts
@@ -1,16 +1,11 @@
-import {  Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { DatabaseModule } from './common/database.module';
-import { ArticleModule } from './modules/articles/article.module'; 
-import { TimelineService } from './modules/timelines/timeline.service';
-
+import { ArticleModule } from './modules/articles/article.module';
 
 @Module({
   imports: [
     DatabaseModule,
-    ArticleModule,
-    TimelineService,
+    ArticleModule
   ],
 })
-export class AppModule {
-  
-}
+export class AppModule {}
