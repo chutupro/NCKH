b8a4123134612961e677dac9a9aff8863a532157
nguyenvanduc6093 <161592322+nguyenvanduc6093@users.noreply.github.com>
2025-10-23 14:43:00 +0700

    312
    132

--- BackEnd/src/app.module.ts
+++ BackEnd/src/app.module.ts
@@ -1,12 +1,14 @@
 import {  Module } from '@nestjs/common';
 import { DatabaseModule } from './common/database.module';
 import { ArticleModule } from './modules/articles/article.module'; 
+import { TimelineService } from './modules/timelines/timeline.service';
 
 
 @Module({
   imports: [
     DatabaseModule,
     ArticleModule,
+    TimelineService,
   ],
 })
 export class AppModule {
--- BackEnd/src/modules/timelines/timeline.module.ts
+++ BackEnd/src/modules/timelines/timeline.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
-import { TimelineController } from './timeline.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Timelines } from '../entities/timeline.entity'; 
 import { TimelineService } from './timeline.service';
+import { TimelineController } from './timeline.controller';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Timelines])],
   controllers: [TimelineController],
   providers: [TimelineService],
+  exports: [TimelineService],
 })
 export class TimelineModule {}
