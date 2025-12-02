d5029b73daa26503bfa2ac75829234a1c133d9c9
doanhphan2110 <doanhphan2110@gmail.com>
2025-12-02 17:49:53 +0700

    .sửa lỗi do Ân-Nghĩa tạo ra bao gồm bộ lọc và nút loading đóng góp

--- FrontEnd/src/config/aiConfig.js
+++ FrontEnd/src/config/aiConfig.js
@@ -3,10 +3,10 @@ const defaultAiConfig = {
   // baseUrls: ['http://192.168.1.87:8000'],//làm việc khi trên công ty hoặc kết nối chung wifi
   baseUrls: ["http://26.68.60.194:8000"], // bật lên khi làm ở nhờ hoặc khi thằng làm ai không có ở đó để test chung
   featureFlags: {
-    analyze: false,
+    analyze: true,
     moderateComment: true,
-    moderateArticle: false,
-    gemini: false,
+    moderateArticle: true,
+    gemini: true,
   },
   endpoints: {
     analyze: "/fast-analyze",
